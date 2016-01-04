20d3ac8f910bbfb1bcd2f6c9683c4eea2ea32765
pauloandremir <miranda.pauloandre@gmail.com>
2016-01-04 17:01:05 -0300

    Commit inicial do projeto

---
+++ xml-com-java/src/br/com/alura/teste/Sistema.java
@@ -0,0 +1,48 @@
+package br.com.alura.teste;
+
+
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import br.com.alura.model.Produto;
+
+public class Sistema {
+	
+	public static void main(String[] args) throws Exception{
+		DocumentBuilderFactory fabrica = DocumentBuilderFactory.newInstance();
+		
+		fabrica.setValidating(true);
+		fabrica.setNamespaceAware(true);
+		fabrica.setAttribute("http://java.sun.com/xml/jaxp/properties/schemaLanguage", "http://www.w3.org/2001/XMLSchema");
+		
+		DocumentBuilder builder = fabrica.newDocumentBuilder();
+		Document document = builder.parse("src/vendas.xml");
+		
+		Element venda = document.getDocumentElement();
+		String moeda = venda.getAttribute("moeda");
+		
+		System.out.println(moeda);
+		
+		NodeList produtos = document.getElementsByTagName("produto");
+		
+		for(int i = 0 ; i < produtos.getLength() ; i++){
+			Element produto = (Element) produtos.item(i);
+			
+			String nome = produto.getElementsByTagName("nome").item(0).getTextContent();
+			double preco = Double.parseDouble(produto.getElementsByTagName("preco").item(0).getTextContent());
+			Produto prod = new Produto(nome, preco);
+			
+			System.out.println(prod);
+		}
+		
+		/*Element  item = (Element) produtos.item(0);
+		String nome = item.getTextContent();
+		System.out.println(nome);*/
+	}
+
+}
